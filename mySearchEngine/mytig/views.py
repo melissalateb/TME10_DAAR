13c95433f239ea019d5ff4cec9ecd7a36e74fe4c
mlateb <latebmelissa@gmail.com>
2025-02-27 11:59:57 +0100

    first commit init

---
+++ mySearchEngine/mytig/views.py
@@ -0,0 +1,73 @@
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from mytig.config import baseUrl
+
+# Create your views here.
+class RedirectionListeDeProduits(APIView):
+    def get(self, request, format=None):
+        response = requests.get(baseUrl+'products/')
+        jsondata = response.json()
+        print("Response1 status code:", response.status_code)
+        print("Response1 text:", response.text)
+        return Response(jsondata)
+#    def post(self, request, format=None):
+#        NO DEFITION of post --> server will return "405 NOT ALLOWED"
+
+class RedirectionDetailProduit(APIView):
+    def get(self, request, pk, format=None):
+        try:
+            response = requests.get(baseUrl+'product/'+str(pk)+'/')
+            jsondata = response.json()
+            print("Response status code:", response.status_code)
+            print("Response text:", response.text)
+            return Response(jsondata)
+        except:
+            raise Http404
+#    def put(self, request, pk, format=None):
+#        NO DEFITION of put --> server will return "405 NOT ALLOWED"
+#    def delete(self, request, pk, format=None):
+#        NO DEFITION of delete --> server will return "405 NOT ALLOWED"
+
+
+
+from mytig.models import ProduitEnPromotion
+from mytig.serializers import ProduitEnPromotionSerializer
+from django.http import Http404
+from django.http import JsonResponse
+
+class PromoList(APIView):
+    def get(self, request, format=None):
+        res=[]
+        for prod in ProduitEnPromotion.objects.all():
+            serializer = ProduitEnPromotionSerializer(prod)
+            response = requests.get(baseUrl+'product/'+str(serializer.data['tigID'])+'/')
+            jsondata = response.json()
+
+            print("Response2 status code:", response.status_code)
+            print("Response2 text:", response.text)
+            res.append(jsondata)
+        return JsonResponse(res, safe=False)
+#    def post(self, request, format=None):
+#        NO DEFITION of post --> server will return "405 NOT ALLOWED"
+
+class PromoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return ProduitEnPromotion.objects.get(pk=pk)
+        except ProduitEnPromotion.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        prod = self.get_object(pk)
+        serializer = ProduitEnPromotionSerializer(prod)
+        response = requests.get(baseUrl+'product/'+str(serializer.data['tigID'])+'/')
+        jsondata = response.json()
+
+        print("Response3 status code:", response.status_code)
+        print("Response3 text:", response.text)
+        return Response(jsondata)
+#    def put(self, request, pk, format=None):
+#        NO DEFITION of put --> server will return "405 NOT ALLOWED"
+#    def delete(self, request, pk, format=None):
+#        NO DEFITION of delete --> server will return "405 NOT ALLOWED"
